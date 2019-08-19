8b6a8222d1c94f4d7f90c5949096f1bcad6cefd1
Lauren Mooney <49599773+laurennmooney@users.noreply.github.com>
2019-08-19 13:56:28 -0400

    components and services started

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { SearchCriteriaComponent } from './search-criteria/search-criteria.component';
+import { EventListComponent } from './search-criteria/event-list/event-list.component';
+import { BucketlistPageComponent } from './bucketlist-page/bucketlist-page.component';
+
+const appRoutes: Routes = [
+  { },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchCriteriaComponent,
+    EventListComponent,
+    BucketlistPageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
