ae0e591f7903599b041be213d1635bf92608d7a2
Lauren Mooney <49599773+laurennmooney@users.noreply.github.com>
2019-08-19 16:20:14 -0400

    search criteria working

--- src/app/event.service.ts
+++ src/app/event.service.ts
@@ -9,9 +9,12 @@ export class EventService {
 
   constructor(private http: HttpClient) { }
   
-
-  getTicketmasterData(keyword: string = ""): Observable<any> {
-    return this.http.get(`https://app.ticketmaster.com/discovery/v2/events.json?size=20&apikey=cXlfgaVOkdGE8RepkWBgQEwQL6FUgYq7&keyword=${keyword}`);
+  getEventData(    
+    keyword: string,
+    city: string,
+    startDate: string,
+    endDate: string): Observable<any> {
+      return this.http.get(`https://app.ticketmaster.com/discovery/v2/events.json?size=20&apikey=cXlfgaVOkdGE8RepkWBgQEwQL6FUgYq7&keyword=${keyword}&locale=*&startDateTime=${startDate}T00:00:00Z&endDateTime=${endDate}T23:59:59Z&city=${city}`);
   }
 
 }
--- src/app/search-criteria/search-criteria.component.ts
+++ src/app/search-criteria/search-criteria.component.ts
@@ -9,15 +9,21 @@ import { NgForm } from '@angular/forms';
 })
 export class SearchCriteriaComponent implements OnInit {
 
-  ticketmasterData: any;
+  eventData: any;
 
   constructor(private eventService: EventService) { }
+
 
   ngOnInit() {
   }
 
-  getDataFromTicketmaster(form: NgForm) {
-    this.eventService.getTicketmasterData(form.value.keyword,).subscribe(response => {
+  getData(    
+    keyword: string,
+    city: string,
+    startDate: string,
+    endDate: string): void {
+    this.eventService.getEventData(keyword, city, startDate, endDate).subscribe(response => {
+      this.eventData = response.data;
       console.log(response);
     });
   }
